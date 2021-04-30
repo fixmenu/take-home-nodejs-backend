4be1c9c90fddd2fbe0e1809f8cbeae1919467cb0
mert gönkesen <mert@yourporter.com>
2021-04-30 13:07:13 +0300

    babel,yarn, nodemon configurations added. Code switch to ES6 syntax

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
-var express = require('express');
-var router = express.Router();
-router.get('/', function(req, res, next) {
-  return res.status(200).json({ message: 'Welcome to Express API template' });
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to Express API template' });
 });
-module.exports = router;
+
+export default router;
