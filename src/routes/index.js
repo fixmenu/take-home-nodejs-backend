c95e9f6df6a4d2550c881184d67a650070ba84b9
mert gönkesen <mert@yourporter.com>
2021-04-30 13:57:16 +0300

    test coverage dependencies added.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import {testEnvironmentVariable} from "../settings";
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to Express API template' });
+  res.status(200).json({ message: testEnvironmentVariable});
 });
 
 export default router;
